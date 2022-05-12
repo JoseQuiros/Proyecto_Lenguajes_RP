81263033340c3b13c17b63146833df2720d9dbc9
HanselCarpio <hcarpio.warlock@gmail.com>
2022-05-12 17:35:26 -0600

    API

---
+++ APILab/APILab/Controllers/LogInController.cs
@@ -0,0 +1,51 @@
+﻿#nullable disable
+using APILab.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace APILab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public partial class LogInController : ControllerBase
+    {
+        private readonly Project_BBBContext _context;
+
+        public LogInController()
+        {
+            _context = new Project_BBBContext();
+        }
+
+        [HttpGet("{email},{password}")]
+        //public async Task<ActionResult<Rol>> LogIn(string email, string password)
+        //{
+        //    var param = new SqlParameter(email, password);
+        //    var rol = 
+
+        //    if (rol == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return rol;
+        //}
+        // GET: api/Students/5
+        //[HttpGet("GetStudentEmail/{email},{id}")]
+        public async Task<ActionResult<Rol>> GetStudentEmail(string email, string password)
+        {
+            var rols = _context.Rols
+                                    .FromSqlInterpolated($@"EXEC LogIn @Email={email}, @Password={password}")
+                                    .AsAsyncEnumerable();
+            await foreach (var rol in rols)
+            {
+                return rol;
+            }
+            return NotFound();
+        }
+
+
+
+
+    }
+}
---
+++ APILab/APILab/Models/Rol.cs
@@ -0,0 +1,8 @@
+﻿namespace APILab.Models
+{
+    public partial class Rol
+    {
+        public int IDrol { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
