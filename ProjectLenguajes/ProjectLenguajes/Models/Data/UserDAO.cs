3945107b8fc96789ecbaa6539485b57ceb9b8de6
JoseQuiros <jqm.25@hotmail.es>
2022-05-11 20:43:52 -0600

    Controller,Data,Domain User

---
+++ ProjectLenguajes/ProjectLenguajes/Models/Data/UserDAO.cs
@@ -0,0 +1,43 @@
+﻿using ProjectLenguajes.Models.Domain;
+using System.Data.SqlClient;
+
+namespace ProjectLenguajes.Models.Data
+{
+    public class UserDAO
+    {
+
+        private readonly IConfiguration _configuration;
+        string connectionString;
+
+        public UserDAO(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public int Insert(User User)
+        {
+
+            return 1;
+        }
+
+      
+        public User Get(string email)
+        {
+            User user = new User();
+        return user;    
+        }
+
+
+        public int Update(User User)
+        {
+         
+
+            return 1;
+
+        }
+    }
+
+
+}
+
