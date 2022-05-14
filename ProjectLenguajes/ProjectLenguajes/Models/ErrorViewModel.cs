c37ab5b36704d33ab95acebd2c8fffe84c2b2fc2
pabloCastillo14 <pity14395@gmail.com>
2022-05-14 02:06:07 -0600

    delete

--- ProjectLenguajes/ProjectLenguajes/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjectLenguajes.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
